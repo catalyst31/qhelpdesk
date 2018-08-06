451876ca54a8d5907e6ed3a40bd48947485218db
catalyst31 <adamcholid311@gmail.com>
2018-08-06 10:56:11 +0700

    cleanup

--- application/views/combo/select_bagian_departemen.php
+++
@@ -1,5 +0,0 @@
-
-<div class="form-group">
-	<label>Sub Department</label>
-	<?php echo form_dropdown('id_bagian_departemen',$dd_bagian_departemen, $id_bagian_departemen, 'required class="form-control"');?>
-</div>
--- application/views/combo/select_sub_kategori.php
+++
@@ -1,5 +0,0 @@
-
-<div class="form-group">
-	<label>Sub Kategori</label>
-	<?php echo form_dropdown('id_sub_kategori',$dd_sub_kategori, $id_sub_kategori, 'required class="form-control"');?>
-</div>
